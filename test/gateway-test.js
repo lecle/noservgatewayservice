aeeb60276efdaaa4665163bdd41f86bc02c3c0f9
daesun <yume@yumes.net>
2015-02-05 17:24:09 +0900

    added gateway logic

--- test/gateway-test.js
+++ test/gateway-test.js
@@ -4,7 +4,6 @@ describe('gateway', function() {
     describe('#init()', function() {
         it('should initialize without error', function(done) {
 
-            // manager service load
             var dummyContainer = {addListener:function(){}};
 
             gateway.init(dummyContainer, function(err) {
@@ -14,4 +13,45 @@ describe('gateway', function() {
         });
     });
 
+    describe('#request()', function() {
+        it('should initialize without error', function(done) {
+
+            var req = {
+                data : {
+                    params : {where : '{"test" : "data"}', method : 'GET', _module : 'TEST'},
+                    body : {test : 'data'}
+                }
+            };
+
+            var res = function(done) {
+
+                return {
+                    send : function() {done();},
+                    error : function(err) {done(err);}
+                };
+            };
+
+            var dummyContainer = {
+                addListener : function(){},
+                getService : function(name) {
+
+                    return {
+                        then : function(callback){ callback({send : function(command, data, callback) {
+
+                            callback(null, {data : {test : 'OK'}});
+                        }});
+
+                            return {fail : function(){}};
+                        }
+                    };
+                }
+            };
+
+            gateway.init(dummyContainer, function(err) {
+
+                gateway.request(req, res(function(){}));
+                gateway.close(done);
+            });
+        });
+    });
 });
---
+++ test/queryParser-test.js
@@ -0,0 +1,30 @@
+var parseQuery = require('../lib/parsers/queryParser');
+var assert = require('assert');
+
+describe('queryParser', function() {
+    describe('#parseQuery()', function() {
+        it('should parse without error', function(done) {
+
+            var sampleData = {
+
+                where : '{"$or" : [{"arr" : {"$nin" : ["b"] } }, {"updatedAt" : {"$ISODate" : "2016-01-23T02:46:54.429Z" } }] }',
+                limit : 2,
+                order : '-num',
+                skip : 2,
+                count : 1
+            };
+
+            var parsedData = parseQuery(sampleData);
+
+            assert(parsedData.where);
+            assert(parsedData.limit);
+            assert(parsedData.order);
+            assert(parsedData.skip);
+            assert(parsedData.count);
+            assert(typeof(parsedData.where.$or[1].updatedAt.getDate()));
+
+            done();
+        });
+    });
+
+});
